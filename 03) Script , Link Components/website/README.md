52f353eb25c3b967d62122c9c261558fc40268c2
chetanck03 <131525183+chetanck03@users.noreply.github.com>
2024-02-25 23:07:36 +0530

    added use

---
+++ 03) Script , Link Components/website/README.md
@@ -0,0 +1,11 @@
+# Link Tag :
+It's used for the stop reloading the pages
+
+# Script Tag :
+It's used for the write the javascript in the jsx
+
+# Image Tag :
+
+It's used for the put image in the webpage or also changing in next.config.mjs
+
+- link :   https://nextjs.org/docs/app/building-your-application/optimizing/images
---
+++ 03) Script , Link Components/website/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+
+    I am Home page
+
+    <Image height={900} width={900} src="https://th.bing.com/th/id/R.6d1a5ec313e6373cc68f2775dadaf44b?rik=fYDMvwywr%2bz6GA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-10%2fSinger-Sidhu-Moose-Wala-Wallpaper-HD-28862.jpg&ehk=x%2fMeRO65vQt5HfZp%2bFO5VNfeuMrlYSsgGS1jApJ6Ago%3d&risl=&pid=ImgRaw&r=0" alt="" />
+   </div>
+  );
+}
